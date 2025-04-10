ae08b5083001fff99276e8a937e16a2c69be728f
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-04-10 15:24:14 -0300

    Add CreateTaskDto, UpdateTaskDto, and Task entity for task management

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsNumber, IsString, Min, MinLength } from "class-validator";
+
+
+export class CreateTaskDto {
+
+    @IsString()
+    @MinLength(5)
+    @IsNotEmpty()
+    readonly name: string;
+
+    @IsString()
+    @MinLength(5)
+    @IsNotEmpty()
+    readonly description: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    userId: number;
+}
---
+++ src/tasks/entities/task.entity.ts
@@ -0,0 +1,9 @@
+
+
+export class Task {
+    id: number;
+    name: string;
+    description: string;
+    completedAt: boolean;
+    createdAt?: Date;
+}
