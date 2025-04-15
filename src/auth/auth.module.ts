b8a4e1f8d596b7087bad3c6f466d097a28d50a42
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-04-15 19:23:03 -0300

    Implement AuthController and AuthService for user authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,32 @@
 import { Global, Module } from '@nestjs/common';
 import { HashingServiceProtocol } from './hash/hashing.service';
 import { BcryptService } from './hash/bcrypt.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Global()
 @Module({
-    exports: [HashingServiceProtocol],
+    imports: [
+        PrismaModule,
+        ConfigModule.forFeature(jwtConfig),
+        JwtModule.registerAsync(jwtConfig.asProvider()),
+    ],
+    exports: [
+        HashingServiceProtocol,
+        JwtModule,
+        ConfigModule
+    ],
     providers: [
         {
             provide: HashingServiceProtocol,
             useClass: BcryptService,
-        }
+        },
+        AuthService
     ],
+    controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule { }
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,58 @@
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { SignInDto } from './dto/signin.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { HashingServiceProtocol } from './hash/hashing.service';
+import jwtConfig from './config/jwt.config';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+
+    constructor(
+        private prisma: PrismaService,
+        private readonly hashingService: HashingServiceProtocol,
+
+        @Inject(jwtConfig.KEY)
+        private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+        private readonly jwtService: JwtService,
+    ) { }
+
+    async authenticate(signInDto: SignInDto) {
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email: signInDto.email,
+            },
+        });
+
+        if (!user) {
+            throw new HttpException('Falha ao fazer login!', HttpStatus.UNAUTHORIZED);
+        }
+
+        const hash = await this.hashingService.compare(signInDto.password, user.passwordHash);
+
+        if (!hash) {
+            throw new HttpException('Senha/Usuario incorretos!', HttpStatus.UNAUTHORIZED);
+        }
+
+        const token = await this.jwtService.signAsync(
+            {
+                sub: user.id,
+                email: user.email,
+            },
+            {
+                secret: this.jwtConfiguration.secret,
+                expiresIn: this.jwtConfiguration.jwtTtl,
+                audience: this.jwtConfiguration.audience,
+                issuer: this.jwtConfiguration.issuer,
+            }
+        )
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token,
+        }
+    }
+}
